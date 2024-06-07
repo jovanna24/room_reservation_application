881e4a58a045948fc7c0c90baaf125e5a96faab2
jovanna24 <77941035+jovanna24@users.noreply.github.com>
2024-06-06 20:45:54 -0700

    added auth helper

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const { Room, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res)=> {
+    
+})
