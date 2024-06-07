0d9e88a993df45cd21351f889f9a20d0c0fe65e5
jovanna24 <77941035+jovanna24@users.noreply.github.com>
2024-06-06 20:21:30 -0700

    added room model

--- models/Room.js
+++ models/Room.js
@@ -1,2 +1,36 @@
 const { Model, DataTypes } = require('sequelize'); 
 const sequelize = require('../config/connection'); 
+const { timeStamp } = require('console');
+
+class Room extends Model {} 
+
+Room.init(
+    {
+        id: {
+        type: DataTypes.INTEGER, 
+        allowNull: false, 
+        primaryKey: true, 
+        autoIncrement: true,
+        }, 
+        room_number: {
+            type: DataTypes.INTEGER, 
+            allowNull: false, 
+        }, 
+        available_resources: {
+            type: DataTypes.STRING, 
+        },
+        room_capacity: {
+            type: DataTypes.NUMBER, 
+            allowNull: false,
+        },
+    }, 
+    {
+        sequelize, 
+        timestamps: false, 
+        freezeTableName: true,
+        underscored: true,
+        modelName:'room',
+    }
+); 
+
+module.exports = Room;
