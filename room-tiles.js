525126231445e35cfec16a09cbcd7c44d1297f61
Morideaux <Morideaux7@gmail.com>
2024-06-06 20:41:29 -0700

    created js to render rooms array using room data can be used in the handlbars vars later

---
+++ room-tiles.js
@@ -0,0 +1,16 @@
+const roomsTiles = document.querySelector("#roomsTiles-section");
+const roomOptions = JSON.parse(localStorage.getItem("roomArray")) || [];
+
+let roomCard = "";
+roomOptions.forEach((room) => {
+  roomCard += `
+    <div class=room-card">
+    <h2>${room.title}</h2>
+    
+    <p>${room.description}</p>
+    <p>${room.features}</p>
+    </div>
+
+`;
+  roomsTiles.innerHTML = roomCard;
+});
