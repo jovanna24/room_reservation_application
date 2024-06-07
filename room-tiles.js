0a7d3df486b4568965eb5478a1d775b5e8c4a18f
Morideaux <Morideaux7@gmail.com>
2024-06-06 20:42:58 -0700

    making sure last commit worked

--- room-tiles.js
+++ room-tiles.js
@@ -6,7 +6,7 @@ roomOptions.forEach((room) => {
   roomCard += `
     <div class=room-card">
     <h2>${room.title}</h2>
-    
+    // <!--this is intended to be the room image but not sure how to fererence one-->
     <p>${room.description}</p>
     <p>${room.features}</p>
     </div>
