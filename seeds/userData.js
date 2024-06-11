7f1e2e0c36b5aa4f2f9b53f98edd5bda7bcf2d7c
Morideaux <Morideaux7@gmail.com>
2024-06-10 19:58:34 -0700

    added admin seed and index info for seeding admin user

---
+++ seeds/userData.js
@@ -0,0 +1,11 @@
+const { User } = require("../models");
+
+const userData = [
+  {
+    id: 1,
+    name: "exampleAdmin",
+    email: "truemail@email.com",
+    password: "deviouscryptic",
+    admin: true,
+  },
+];
