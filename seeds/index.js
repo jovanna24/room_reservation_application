e1a08efc55dccd081992a09a3a6b61f8da0d9fc8
jovanna24 <77941035+jovanna24@users.noreply.github.com>
2024-06-14 19:56:14 -0700

    added index seed file

--- seeds/index.js
+++ seeds/index.js
@@ -1,20 +1,47 @@
-const sequelize = require("../config/connection");
-const seedUserData = require("./userData");
+const sequelize = require('../config/connection');
+const { User, Event, Room } = require('../models');
 
-const seedAll = async () => {
-  try {
-    await sequelize.sync({ force: true });
-    console.log("Database synced successfully.");
+const userData = require('./userData.json');
+const eventData = require('./eventData.json'); 
+const roomData = require('./roomData.json')
 
-    await seedUserData();
-    console.log("User data seeded successfully.");
+const seedDatabase = async () => {
+    try {
+        await sequelize.sync({ force: true });
+        console.log('Database synced!');
 
+        const users = await User.bulkCreate(userData, {
+            individualHooks: true, 
+            returning: true,
+        }); 
+        console.log('Users seeded!');
+
+        const events = await Promise.all(
+            eventData.map(async (event) => {
+                const createdEvent = await Event.create({
+                    ...event, 
+                    user_id: users[Math.floor(Math.random() * users.length)].id,
+                }); 
+                return createdEvent;
+            })
+        );
+        console.log('Events seeded!'); 
+
+        await Promise.all(
+            roomData.map(async (room) => {
+                return await Room.create({
+                    ...room, 
+                    user_id: users[Math.floor(Math.random() * users.length)].id,
+                    event_id: events[Math.floor(Math.random() * events.length)].id,
+                });
+            })
+        );
+        console.log('Rooms seeded!');
+        console.log('Database seeded successfully!');
+    } catch (err) {
+        console.error('Failed to seed database: ', err);
+    } 
     process.exit(0);
-  } catch (err) {
-    console.error("Failed to seed the database:", err);
-    process.exit(1);
-  }
 };
 
-seedAll();
-
+seedDatabase();
