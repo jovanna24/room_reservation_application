c8cad9530d61daa94906eccd54486ffe1a2d6536
jovanna24 <77941035+jovanna24@users.noreply.github.com>
2024-06-17 17:02:29 -0700

    fixed seeding and routing issue encountered in render

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,12 @@ if (process.env.DB_URL) {
   sequelize = new Sequelize(process.env.DB_URL);
 } else {
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD, 
+    // process.env.DB_NAME,
+    // process.env.DB_USER,
+    // process.env.DB_PASSWORD, 
+    DB_NAME='room_reservation_db',
+    DB_USER='postgres',
+    DB_PASSWORD='Dxsxxd1124!',
     {
       host: 'localhost',
       dialect: 'postgres',
